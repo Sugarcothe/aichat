8792030a4dc0fea14a9e3d1ad6921a336b34db83
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-07-26 20:11:34 +0100

    landing page completed

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -23,7 +23,7 @@ const RootLayout = () => {
         <header>
           <Link to={"/"} className="logo">
             <img src="/logo.png" alt="" />
-            <span>AICHAT</span>
+            <span>CuriousCat</span>
           </Link>
           <div className="user">
             <SignedIn>
--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -5,9 +5,23 @@ import { Link } from "react-router-dom";
 const Homepage = () => {
   return (
     <div className="homepage">
-      <Link to={"/dashboard"} className="dashboard">
-        Dashboard
-      </Link>
+      <div className="left">
+        <h1>Curious Cat</h1>
+        <h2>Lorem ipsum dolor sit amet Fugiat, maiores.</h2>
+        <h3>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
+          itaque doloremque quo voluptatum! Dolorum tenetur adipisci sint
+          tempore tempora itaque aliquid. Ex explicabo veritatis praesentium
+          sequi quae, exercitationem consequuntur.
+        </h3>
+        <Link to={"/dashboard"} className="button">
+          Get started
+        </Link>
+      </div>
+      <div className="right">
+        <img src="./curioscat.png" alt="curios-cat" />
+      </div>
+      
     </div>
   );
 };
